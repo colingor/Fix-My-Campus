1229ec369acece3ecb1e6bc6d01c63725768fe83
Colin Gormley <colin.gormley@ed.ac.uk>
2015-05-11 10:49:24 +0100

    Added missing file

---
+++ Estates Audit/ReportDatabaseAvailability.h
@@ -0,0 +1,15 @@
+//
+//  ReportDatabaseAvailability.h
+//  Estates Audit
+//
+//  Created by Colin Gormley on 07/05/2015.
+//  Copyright (c) 2014 EDINA. All rights reserved.
+//
+
+#ifndef Estates_Audit_ReportDatabaseAvailability_h
+#define Estates_Audit_ReportDatabaseAvailability_h
+
+#define ReportDatabaseAvailabilityNotification @"ReportDatabaseAvailabilityNotification"
+#define ReportDatabaseAvailabilityContext @"Context"
+
+#endif
