e1469211609202f9949af32c4c5462357bcd903a
Ian Fieldhouse <me@ianfieldhouse.com>
2015-05-12 15:32:31 +0100

    Issue #15 Add LocationDetailsViewController class and hook up storyboard

---
+++ Estates Audit/Controller/LocationDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDetailsViewController.h
+//  Estates Audit
+//
+//  Created by Ian Fieldhouse on 12/05/2015.
+//  Copyright (c) 2015 Colin Gormley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationDetailsViewController : UIViewController
+
+@end
