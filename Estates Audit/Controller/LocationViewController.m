90d0e8ac8276349ad0670e669386c96c509f50fd
Colin Gormley <colin.gormley@ed.ac.uk>
2015-06-11 14:55:57 +0100

    Issue #15: If user selects a building on the list, the map will now centre on it (and expand the appropriate annotation). Coordinates also saved in Core Data

--- Estates Audit/Controller/LocationViewController.m
+++ Estates Audit/Controller/LocationViewController.m
@@ -200,6 +200,25 @@
     NSMutableString *text;
     text = [self generateDescriptionFromAnnotation:point];
     self.descriptionText.text = text;
+    
+    self.locationPin.coordinate = point.coordinate;
+    
+    CLLocationDegrees lat = point.coordinate.latitude;
+    CLLocationDegrees lon = point.coordinate.longitude;
+    
+    [self.reportDict setValue:[NSNumber numberWithDouble:lat] forKey:@"lat"];
+    [self.reportDict setValue:[NSNumber numberWithDouble:lon] forKey:@"lon"];
+    
+    // Centre map on point
+    MKCoordinateRegion region = { { 0.0, 0.0 }, { 0.0, 0.0 } };
+    region.center.latitude = lat;
+    region.center.longitude = lon;
+    region.span.latitudeDelta = 0.0003;
+    region.span.longitudeDelta = 0.0003;
+    [self.mapView setRegion:region animated:YES];
+    
+    [self.mapView selectAnnotation:point animated:YES];
+    
 }
 
 - (IBAction)toggleBuildingsView:(id)sender {
