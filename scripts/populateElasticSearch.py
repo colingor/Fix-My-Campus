d2e9383bc9cec1e92bd51223223b0d17496c8735
Colin Gormley <colin.gormley@ed.ac.uk>
2015-08-19 11:53:06 +0100

    Issue #121: Modified script to populate ElasticSearch with new mapping.
    This adds a mapping supporting nested building information to the ‘buildings’ index (rather than ‘estates’ which is currently being used by the app).  The buildings listed in the original estates.json file are added into this new format, along with the nested sample that was created in uoe-estates-buildings.json.

--- scripts/populateElasticSearch.py
+++ scripts/populateElasticSearch.py
@@ -13,8 +13,10 @@ results = parser.parse_args()
 
 class PopulateElasticSearch:
 
-   INDEX_NAME = 'estates'
-   BUILDING_JSON = 'estates.json'
+   # INDEX_NAME = 'estates'
+   INDEX_NAME = 'buildings'
+   BUILDING2_JSON = 'estates.json'
+   BUILDING_JSON = 'uoe-estates-buildings.json'
 
    ES_HOST = 'dlib-brown.edina.ac.uk'
    ES_PORT = '9200'
@@ -22,22 +24,57 @@ class PopulateElasticSearch:
    def __init__(self):
        self.es = Elasticsearch([{'host': self.ES_HOST, 'port': self.ES_PORT}])
 
+
    def loadEstatesBuildings(self):
 
+       i = 1
        with open(self.BUILDING_JSON, 'rU') as data_file:
           data = json.load(data_file)
-          i = 1
+          # for loc in data['locations']:
+          for loc in data['features']:
+              # Have to modify the locations to ensure they're stored in the correct format for es spatial searching
+              loc['geometry']['location'] =  loc['geometry'].pop('coordinates')
+
+              # Add to index
+              self.es.index(index=self.INDEX_NAME, doc_type='building', id=i, body=loc)
+              i += 1
+
+       with open(self.BUILDING2_JSON, 'rU') as data_file:
+          data = json.load(data_file)
           for loc in data['locations']:
               # Have to modify the locations to ensure they're stored in the correct format for es spatial searching
               name = loc.get('name');
               if name:
                   latitude = loc['latitude'];
                   longitude = loc['longitude'];
-                  location = ('%s, %s' % (latitude, longitude))
-                  loc['location'] = location;
+
+                  # Populate expected template
+                  location = {
+                     "geometry":{
+                        "location":[float(longitude), float(latitude)],
+                         "type": "Point"
+                      },
+                     "properties":{
+                         "area": loc.get('campuses'),
+                         "image":"",
+                         "subtitle": loc.get('address'),
+                         "title": name,
+                         "information":[{
+                                 "area":"General",
+                                 "items":[{
+                                     "description":"",
+                                     "image": "",
+                                     "notes": "",
+                                     "type" : ""
+                                     }]
+                        }]
+                      },
+                     "type":"Feature"
+                     }
+
 
                   # Add to index
-                  self.es.index(index=self.INDEX_NAME, doc_type='location', id=i, body=loc)
+                  self.es.index(index=self.INDEX_NAME, doc_type='building', id=i, body=location)
                   i += 1
 
 
@@ -49,33 +86,96 @@ class PopulateElasticSearch:
          # Ignore exception caused when no index to delete
          pass
 
-     # have to specify mapping so geo_points are indexed correctly
+      # have to specify mapping so geo_points are indexed correctly
+#      mapping = {
+#              "mappings": {
+#                  "location": {
+#                      "properties": {
+#                          "abbreviation": {
+#                              "type": "string"
+#                              },
+#                          "address": {
+#                              "type": "string"
+#                              },
+#                          "campuses": {
+#                              "type": "string"
+#                              },
+#                          "categories": {
+#                              "type": "string"
+#                              },
+#                          "location": {
+#                              "type": "geo_point"
+#                              },
+#                          "name": {
+#                              "type": "string"
+#                              }
+#                          }
+#                      }
+#                  }
+#              }
+
+
      mapping = {
              "mappings": {
-                 "location": {
+                 "building": {
                      "properties": {
-                         "abbreviation": {
-                             "type": "string"
+                         "geometry": {
+                             "properties": {
+                                 "location": {
+                                     "type": "geo_point"
+                                     },
+                                 "type": {
+                                     "type": "string"
+                                     }
+                                 }
                              },
-                         "address": {
-                             "type": "string"
+                         "properties": {
+                             "properties": {
+                                 "area": {
+                                     "type": "string"
+                                     },
+                                 "image": {
+                                     "type": "string"
+                                     },
+                                 "information": {
+                                     "properties": {
+                                         "area": {
+                                             "type": "string"
+                                             },
+                                         "items": {
+                                             "type": "nested",
+                                             "properties": {
+                                                 "description": {
+                                                     "type": "string"
+                                                     },
+                                                 "image": {
+                                                     "type": "string"
+                                                     },
+                                                 "notes": {
+                                                     "type": "string"
+                                                     },
+                                                 "type": {
+                                                     "type": "string"
+                                                     }
+                                                 }
+                                             }
+                                         }
+                                     },
+                                 "subtitle": {
+                                     "type": "string"
+                                     },
+                                 "title": {
+                                     "type": "string"
+                                     }
+                                 }
                              },
-                         "campuses": {
-                             "type": "string"
-                             },
-                         "categories": {
-                             "type": "string"
-                             },
-                         "location": {
-                             "type": "geo_point"
-                             },
-                         "name": {
-                             "type": "string"
-                             }
+                         "type": {
+                                 "type": "string"
+                                 }
                          }
-                     }
-                 }
-             }
+      }
+   }
+}
 
      self.es.indices.create(index=self.INDEX_NAME, body=mapping)
 
@@ -84,7 +184,7 @@ loader = PopulateElasticSearch()
 loader.recreateElasticSearchIndex()
 loader.loadEstatesBuildings()
 
-# print loader.es.get(index=loader.INDEX_NAME, doc_type="location", id=42)
+# print loader.es.get(index=loader.INDEX_NAME, doc_type="building", id=1)
 time.sleep(2)
 res = loader.es.search(index=loader.INDEX_NAME, body={"query": {"match_all": {}}})
 print("Done : Indexed %d buildings" % res['hits']['total'])
